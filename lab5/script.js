8cdaf05bfbdc0adb74ef5443ecc2bd20e57a7d44
Nikita <nikitamavrenko@yandex.ru>
2024-10-04 21:48:31 +0300

    lab5

---
+++ lab5/script.js
@@ -0,0 +1,36 @@
+const products = {
+    'Товар 1': 100,
+    'Товар 2': 150,
+    'Товар 3': 200,
+}
+
+const productSelect = document.getElementById('product')
+
+for (let product in products) {
+    const option = document.createElement('option')
+    option.value = products[product]
+    option.textContent = `${product} - ${products[product]} руб`
+    productSelect.appendChild(option)
+}
+
+const calculateBtn = document.getElementById('calculateBtn')
+
+calculateBtn.addEventListener('click', event => {
+    event.preventDefault()
+    const productSelect = document.getElementById('product')
+    const quantityInput = document.getElementById('quantity')
+    const resultDiv = document.getElementById('result')
+
+    const price = productSelect.value
+    const quantity = quantityInput.value
+
+    const regex = /^\d+$/
+
+    if (!regex.test(quantity)) {
+        resultDiv.textContent = 'Некорректное значение'
+        return
+    }
+
+    const totalCost = price * quantity
+    resultDiv.textContent = `Стоимость вашего заказа: ${totalCost} руб.`
+})
